8f5a2c64e9724656a36dc68f540edfb7462d94ec
vafle <vafle@ARTURIK>
2023-05-29 21:51:48 +0300

    ss

--- SMFinder/SMFinder/App.xaml.cs
+++ SMFinder/SMFinder/App.xaml.cs
@@ -1,11 +1,28 @@
-﻿using System;
+﻿using SMFinder.Models;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.IO;
 
 namespace SMFinder
 {
     public partial class App : Application
     {
+        public const string DATABASE_NAME = "friends.db";
+        public static   UserRepository database;
+        public static UserRepository Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new UserRepository(
+                        Path.Combine(
+                            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), DATABASE_NAME));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
